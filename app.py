fc8047ff368a17d679134858cbd37d77fa1b4d27
Manas Apte <mapte@mapte.twilio.internal>
2013-10-25 15:52:46 -0700

    committing stuff

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, Response, session, abort, jsonify
 import json
+import redis
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+r = redis.StrictRedis(host='localhost',port=6379,db=0)
+
 @app.route('/twipic')
 def index():
-    return render_template('picture.html') 
+    id = r.incr('sessionid')
+    return render_template('picture.html',id=id) 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8000)
---
+++ static/js/twilio-picture.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#closemodal').click(function(){
+    $('#myModal').modal('hide'); 
+  });
+  $('#myModal').modal();
+  $('#error-name, #error-phone').hide();
+  $("#tetris-play").click(
+    if($('#twipic-name').val().trim() == '') {
+      $('#error-name).show();
+    }
+    
+  );
+});
+
+
